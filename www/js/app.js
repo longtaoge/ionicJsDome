98799b2ea197d782d5fdb4865cbc35e61fbab883
longtaoge <61852263@qq.com>
2015-02-02 10:39:16 +0800

    添加事件示例

--- www/js/app.js
+++ www/js/app.js
@@ -21,8 +21,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvide) {
+        $ionicConfigProvider.platform.android.tabs.position("bottom");
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,65 +1,62 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function ($scope) {
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    .controller('FriendsCtrl', function ($scope, Friends) {
+        $scope.friends = Friends.all();
+    })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+    .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
-
-
-
-    .controller('JsCtrl', function($scope) {
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    })
 
 
+    .controller('JsCtrl', function ($scope) {
 
 
     })
 
 
+    .controller('ActionSheetCtrl', function ($scope, $ionicActionSheet, $timeout) {  /*ActionSheet 示例的控制器*/
 
-    .controller('ActionSheetCtrl', function($scope, $ionicActionSheet, $timeout) {  /*ActionSheet 示例的控制器*/
-
-        $scope.show = function() { //展示
+        $scope.show = function () { //展示
             // Show the action sheet
             var hideSheet = $ionicActionSheet.show({
                 buttons: [
-                    { text: '<b>分</b> 享' },
-                    { text: '移除' }
+                    {text: '<b>分</b> 享'},
+                    {text: '移除'}
                 ],
                 destructiveText: '删除',
                 titleText: '标题',
                 cancelText: '取消',
-                cancel: function() {
+                cancel: function () {
                     // 可以添加方法
                 },
-                buttonClicked: function(index) {
+                buttonClicked: function (index) {
                     return true;
                 }
             });
 
             // 设置超时后自动关闭
-            $timeout(function() {
+            $timeout(function () {
                 hideSheet();
             }, 2000);
 
@@ -67,31 +64,67 @@ angular.module('starter.controllers', [])
 
     })
 
-    .controller('BackdropCtrl', function($scope, $ionicBackdrop, $timeout) {
+    .controller('BackdropCtrl', function ($scope, $ionicBackdrop, $timeout) {
         //显示一个阴影背景，时长一秒钟
-        $scope.action = function() { // 通过方法调用
+        $scope.action = function () { // 通过方法调用
             $ionicBackdrop.retain();// 显示
-            $timeout(function() { //设置超时时间
+            $timeout(function () { //设置超时时间
                 $ionicBackdrop.release(); //关闭
             }, 1000);
         };
     })
 
-    .controller('ContentCtrl', function($scope) {
+    .controller('ContentCtrl', function ($scope) {
 
+        /*content*/
 
-    /*content*/
+    }).controller('EventsCtrl', function ($scope) {
 
-
-    })    .controller('EventsCtrl', function($scope) {
-
-
-
+        $scope.onHold = function () {
+            alert("onHold");
+        };
+        $scope.onTap = function () {
+            alert("onTap");
+        };
+        $scope.onTouch = function () {
+            alert("onTouch");
+        };
+        $scope.onRelease = function () {
+            alert("onRelease");
+        };
+        $scope.onDrag = function () {
+            alert("onDrag");
+        };
+        $scope.onDragUp = function () {
+            alert("onDragUp");
+        };
+        $scope.onDragRight = function () {
+            alert("onDragRight");
+        };
+        $scope.onDragDown = function () {
+            alert("onDragDown");
+        };
+        $scope.onDragLeft = function () {
+            alert("onDragLeft");
+        };
+        $scope.onSwipe = function () {
+            alert("onSwipe");
+        };
+        $scope.onSwipeUp = function () {
+            alert("onSwipeUp");
+        };
+        $scope.onSwipeDown = function () {
+            alert("onSwipeDown");
+        };
+        $scope.onSwipeRight = function () {
+            alert("onSwipeRight");
+        };
+        $scope.onSwipeLeft = function () {
+            alert("onSwipeLeft");
+        };
 
 
     })
-
-
 
 
 ;
