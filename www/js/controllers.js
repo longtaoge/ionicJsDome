57b2cab82be30f45ef6ba823e12f7c5ffe5f7c38
longtaoge <61852263@qq.com>
2015-02-03 00:31:52 +0800

    添加截图

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,31 +3,6 @@ angular.module('starter.controllers', [])
     .controller('DashCtrl', function ($scope) {
     })
 
-    .controller('ChatsCtrl', function ($scope, Chats) {
-        $scope.chats = Chats.all();
-        $scope.remove = function (chat) {
-            Chats.remove(chat);
-        }
-    })
-
-    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-        $scope.chat = Chats.get($stateParams.chatId);
-    })
-
-    .controller('FriendsCtrl', function ($scope, Friends) {
-        $scope.friends = Friends.all();
-    })
-
-    .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
-        $scope.friend = Friends.get($stateParams.friendId);
-    })
-
-    .controller('AccountCtrl', function ($scope) {
-        $scope.settings = {
-            enableFriends: true
-        };
-    })
-
 
     .controller('JsCtrl', function ($scope) {
 
@@ -78,7 +53,7 @@ angular.module('starter.controllers', [])
 
         /*content*/
 
-    }).controller('EventsCtrl', function ($scope) {
+    }).controller('EventsCtrl', function ($scope) {  /*events 控制器*/
 
         $scope.onHold = function () {
             alert("onHold");
