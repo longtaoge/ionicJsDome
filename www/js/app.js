0526c9a8324ffddab2e9fdb0f30742f95897dea4
longtaoge <61852263@qq.com>
2015-03-02 17:34:24 +0800

    添加下拉刷新

--- www/js/app.js
+++ www/js/app.js
@@ -7,124 +7,119 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
         $ionicConfigProvider.platform.android.tabs.position("bottom");
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
 
-  // Each tab has its own nav history stack:
+            // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
 
 
+            .state('tab.js', {//JS 测试的Tab
+                url: '/js',
+                abstract: true,
+                views: {
+                    'tab-js': {
+                        templateUrl: 'templates/tab-js.html',
+                        controller: 'JsCtrl'
+                    }
+                }
+            })
+
+            .state('tab.js.actionsheet', { //actionseet  活动表格 测试
+                url: "/actionsheet",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/action-sheet.html"
+                        , controller: 'ActionSheetCtrl'
+                    }
+                }
+            })
+            .state('tab.js.backdrop', {//backdrop 背景变暗测试
+                url: "/backdrop",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/backdrop.html"
+                        , controller: 'BackdropCtrl'
+                    }
+                }
+            })
+
+            .state('tab.js.content', { //content 组件测试
+                url: "/content",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/content.html"
+                        , controller: 'ContentCtrl'
+                    }
+                }
+            })
+            .state('tab.js.events', { //events 测试
+                url: "/events",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/events.html"
+                        , controller: 'EventsCtrl'
+                    }
+                }
+            })
+            .state('tab.js.ion_refresher', { //events 测试
+                url: "/ion_refresher",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/ion_refresher.html"
+                        , controller: 'IonRefresherCtrl'
+                    }
+                }
+            })
+    .state('tab.js.ion_infinite_scroll', { //events 测试
+                url: "/ion_infinite_scroll",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/jsdome/ion_infinite_scroll.html"
+                        , controller: 'InfiniteScrollCtrl'
+                    }
+                }
+            })
+
+        ;
 
 
-      .state('tab.js', {//JS 测试的Tab
-          url: '/js',
-          abstract: true,
-          views: {
-              'tab-js': {
-                  templateUrl: 'templates/tab-js.html',
-                  controller: 'JsCtrl'
-              }
-          }
-      })
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/dash');
 
-      .state('tab.js.actionsheet', { //actionseet  活动表格 测试
-          url: "/actionsheet",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/jsdome/action-sheet.html"
-                  ,controller:'ActionSheetCtrl'
-              }
-          }
-      })
-      .state('tab.js.backdrop', {//backdrop 背景变暗测试
-          url: "/backdrop",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/jsdome/backdrop.html"
-                  ,controller:'BackdropCtrl'
-              }
-          }
-      })
-
-      .state('tab.js.content', { //content 组件测试
-          url: "/content",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/jsdome/content.html"
-                  ,controller:'ContentCtrl'
-              }
-          }
-      })
-      .state('tab.js.events', { //events 测试
-          url: "/events",
-          views: {
-              'menuContent': {
-                  templateUrl: "templates/jsdome/events.html"
-                  ,controller:'EventsCtrl'
-              }
-          }
-      })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  ;
-
-
-
-
-
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,6 @@ angular.module('starter.controllers', [])
 
 
     .controller('ActionSheetCtrl', function ($scope, $ionicActionSheet, $timeout) {  /*ActionSheet 示例的控制器*/
-
         $scope.show = function () { //展示
             // Show the action sheet
             var hideSheet = $ionicActionSheet.show({
@@ -29,14 +28,11 @@ angular.module('starter.controllers', [])
                     return true;
                 }
             });
-
             // 设置超时后自动关闭
             $timeout(function () {
                 hideSheet();
             }, 2000);
-
         };
-
     })
 
     .controller('BackdropCtrl', function ($scope, $ionicBackdrop, $timeout) {
@@ -51,9 +47,10 @@ angular.module('starter.controllers', [])
 
     .controller('ContentCtrl', function ($scope) {
 
-        /*content*/
+        /*content 控制器*/
 
-    }).controller('EventsCtrl', function ($scope) {  /*events 控制器*/
+    })
+    .controller('EventsCtrl', function ($scope) {  /*events 控制器*/
 
         $scope.onHold = function () {
             alert("onHold");
@@ -98,8 +95,67 @@ angular.module('starter.controllers', [])
             alert("onSwipeLeft");
         };
 
-
     })
+    .controller('IonRefresherCtrl', function ($scope, $http, $timeout) {
+        $scope.items = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11];
+        $scope.doRefresh = function () {
+            // alert("正在执行，下拉刷新");
+            $http.get('/new-items') //联网请求数据
+                .success(function (newItems) {
+                    $scope.items = newItems;
+                })
+                .finally(function () {
+                    //停止下拉进度条
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        };
 
+        $scope.onPulling=function(){ //下拉时调用的方法
+           // alert("onpulling");
+        };
+    })
+    .controller('InfiniteScrollCtrl', function ($scope, $http, $timeout) {
+        //     $scope.items = [1, 2, 3,5,6,7,8,9,10,11];
+
+        $scope.doRefresh = function () {
+            // alert("正在执行，下拉刷新");
+            $http.get('/new-items') //联网请求数据
+                .success(function (newItems) {
+                    $scope.items = newItems;
+                })
+                .finally(function () {
+                    //停止下拉进度条
+                    // Stop the ion-refresher from spinning
+
+                    $scope.$broadcast('scroll.refreshComplete');
+
+
+                });
+        };
+
+
+        $scope.loadMore = function () {
+          //  alert("loadmore");
+            $scope.loading = false;
+            $scope.noMoreItemsAvailable = false;
+            // alert("正在执行，下拉刷新");
+            $http.get('/new-items') //联网请求数据
+                .success(function (newItems) {
+                    $scope.items = newItems;
+                })
+                .finally(function () {
+                    $timeout(function () {
+                        $scope.items.push({id: $scope.items.length});
+                        if ($scope.items.length == 30) {
+                            $scope.noMoreItemsAvailable = true;
+                        }
+                        $scope.$broadcast('scroll.infiniteScrollComplete');
+                    }, 1000);
+                });
+
+
+        };
+        $scope.items = [];
+    })
 
 ;
